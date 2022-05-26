e1410c834da9125850afdc932c515eb61b72f586
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-05-26 09:36:14 -0700

    052622A

--- module-1/03_Expressions/exercise-student/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/03_Expressions/exercise-student/java/src/main/java/com/techelevator/Exercises.java
@@ -324,6 +324,12 @@ public class Exercises {
 	 squirrelPlay(95, true) → true
 	 */
 	public boolean squirrelPlay(int temp, boolean isSummer) {
+		if (temp>=60 && temp<=90){
+			return true;
+		}
+		else if(isSummer == true && temp>=60 && temp<=100){
+			return true;
+		}
 		return false;
 	}
 
