3e871ddc4d59032e9f934492c88e602ba35d1842
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-05-31 21:48:59 -0700

    053122A

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
@@ -11,6 +13,19 @@ public class TempConvert {
 
 		System.out.print("Is the temperature in (C)elsiusk or (F)ahrenheit?: ");
 		String scaleInput= userInput.nextLine();
+		if("F".equals(scaleInput)) {
+			int temperatureCelsius = (int) ((temperature - 32) / 1.8);
+			System.out.println(temperature + " F is " + temperatureCelsius + " C.");
+		}
+		else if("C".equals(scaleInput)){
+			int temperatureFah = (int)((temperature*1.8)+32);
+			System.out.println(temperature+ "C is "+temperatureFah+" F.");
+
+		}
+
+
+
+
 
 	}
 
