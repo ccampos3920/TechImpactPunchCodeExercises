eda54e1b6c04b201df840db6e90e44878cd2764a
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-06-01 14:55:06 -0700

    060122A

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,21 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
+
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter a number: ");
+		String integerInput = userInput.nextLine();
+		int number = Integer.parseInt(integerInput);
+		int a = 0;
+		for(int i=0; i<=number; i++){
+			a = i;
+			a = a+i;
+			System.out.print(i + " ");
+		}
 
 	}
 
