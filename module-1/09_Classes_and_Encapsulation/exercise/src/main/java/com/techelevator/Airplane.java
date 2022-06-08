1e36c1b505cd87a9ad17bb5da970f166f94675db
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-06-08 12:50:16 -0700

    060822A

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -1,5 +1,66 @@
 package com.techelevator;
 
 public class Airplane {
+    private String planeNumber;
+    private int totalFirstClassSeats;
+    private int bookedFirstClassSeats;
+    private int totalCoachSeats = 0;
+    private int BookedCoachedSeats = 0;
+    private boolean forFirstClass;
+    private int availableCoachSeats;
 
-}
+    public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats){
+        this.planeNumber = planeNumber;
+        this.totalFirstClassSeats= totalFirstClassSeats;
+        this.totalCoachSeats = totalCoachSeats;
+    }
+
+    public String getPlaneNumber(){
+        return planeNumber;
+    }
+
+    public int getTotalFirstClassSeats(){
+        return totalFirstClassSeats;
+    }
+
+    public int getTotalCoachSeats(){
+        return totalCoachSeats;
+    }
+
+        public int getBookedFirstClassSeats(){
+        return bookedFirstClassSeats;
+    }
+
+    public int getBookedCoachedSeats(){
+        return BookedCoachedSeats;
+    }
+
+    public boolean getforFirstClass(){
+        return forFirstClass;
+    }
+    public int getAvailableCoachSeats(){
+        return availableCoachSeats;
+    }
+    public int reserveSeats(boolean forFirstClass, int totalNumberOfSeats){
+        if(forFirstClass==true){
+            bookedFirstClassSeats += totalNumberOfSeats;
+            return bookedFirstClassSeats;
+        }
+
+        else if(forFirstClass==false){
+            BookedCoachedSeats+= totalNumberOfSeats;
+            return BookedCoachedSeats;
+        }
+
+        return totalNumberOfSeats;
+
+    }
+
+    }
+
+
+
+
+
+
+
