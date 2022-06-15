db345ec3892d45501285b9265400d1c836a40705
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-06-15 14:04:34 -0700

    061522A

--- module-1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/SquareWall.java
+++ module-1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/SquareWall.java
@@ -9,11 +9,7 @@ public class SquareWall extends RectangleWall{
         super(name,color, sideLength, sideLength);
     }
 
-    public int getSideLength(){
-        return sideLength;
-    }
-
-
+    
     public String toString(){
         return name+" ("+sideLength+"x"+sideLength+") square";
     }
