f5c3ec3bdf48cc4ff2efc671e2f1abe8c6e1df95
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-06-21 14:24:01 -0700

    062122A

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -2,26 +2,40 @@ package com.techelevator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Locale;
+
 
 public class WordSearch {
 
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("What is the fully qualified name of the file that should be searched? ");
-		String fileName = sc.nextLine();
 
-		System.out.println("What is the search word you are looking for?");
+		System.out.println("What is the fully qualified name of the file that should be searched? ");
+		String filePath = sc.nextLine();
+
+		System.out.println("What is the search word you are looking for? ");
 		String word = sc.nextLine();
 
-		File file = new File(fileName);
-		Scanner dataInput = new Scanner(fileName);
+		File file = new File(filePath);
 		int i = 1;
 
-		while(dataInput.hasNextLine()){
-		String lineOfInput = dataInput.nextLine();
-		System.out.println(i + " " + lineOfInput);
-		i++;
+		try (Scanner fileInput = new Scanner(file)) {
+			while (fileInput.hasNextLine()) {
+				String lineOfInput = fileInput.nextLine();
+				i++;
+
+				if (lineOfInput.contains(word)) {
+					System.out.println(i + ") " + lineOfInput);
+				}
+			}
+
+
+		}
+		catch (FileNotFoundException e) {
+				// Could not find the file at the specified path
+				System.out.println("The file was not found: " + filePath);
+
 		}
 
 	}
