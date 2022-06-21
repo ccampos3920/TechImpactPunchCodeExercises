b7a2618aee5c38f4825433bfc3cb61f1aa0bb902
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-06-21 10:16:27 -0700

    062122A

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,8 +1,20 @@
 package com.techelevator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class WordSearch {
 
+	Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
+		File file = new File("alices_adventures_in_wonderland.txt");
+		Scanner dataInput = new Scanner("alices_adventures_in_wonderland.txt");
+
+		while(!dataInput.hasNextLine()){
+		String lineOfInput = dataInput.nextLine();
+		System.out.println(lineOfInput + " ");
+		}
 
 	}
 
