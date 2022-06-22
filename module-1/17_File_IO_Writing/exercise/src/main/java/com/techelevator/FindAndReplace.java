6ed5e5ce8c628fbdbc1faf46b83d976ee589d8c9
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-06-22 14:52:11 -0700

    062222A

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,46 @@
 package com.techelevator;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.*;
 
 public class FindAndReplace {
 
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // User questions/input
+    System.out.println("What is the search word? ");
+    String sWord = sc.nextLine();
+
+    System.out.println("What is the replacement word? ");
+    String rWord = sc.nextLine();
+
+    System.out.println("What is the source file? ");
+    String sFile = sc.nextLine();
+
+    System.out.println("What is the destination file? ");
+    String dFile = sc.nextLine();
+
+    File sourceFile = new File(sFile);
+    File destinationFile  = new File(dFile);
+
+    try(Scanner dataInput = new Scanner(sourceFile.getAbsoluteFile());
+        PrintWriter dataOutput = new PrintWriter(destinationFile.getAbsoluteFile())){
+            while(dataInput.hasNextLine()){
+                String lineOfInput = dataInput.nextLine();
+                dataOutput.println(lineOfInput.replaceAll(sWord, rWord));
+
+            }
 
     }
+    catch (FileNotFoundException e) {
+        System.out.println("The file " + sFile + " Was not found, please try again");
+    }
+
+    }
+
 
 }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,45 @@
 package com.techelevator;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.*;
 
 public class FizzWriter {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("What is the destination file?");
+		String dFile = sc.nextLine();
+
+		File destinationFile = new File(dFile);
+
+
+		try(PrintWriter dataOutput = new PrintWriter(destinationFile.getAbsoluteFile())){
+			for(int i = 1; i<301; i++){
+				if(i%3==0 && i%5==0){
+					dataOutput.println("FizzBuzz");
+				}
+				else if(i%3==0){
+					dataOutput.println("Fizz");
+				}
+				else if(i%5==0){
+					dataOutput.println("Buzz");
+				}
+				else{
+					dataOutput.println(i);
+				}
+			}
+
+		}
+		catch(FileNotFoundException e){
+			System.out.println("The destination file "+ dFile + " is not found, please try again.");
+		}
 
 	}
 
 }
+
