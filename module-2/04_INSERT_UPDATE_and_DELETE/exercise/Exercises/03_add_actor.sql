8c75898764d1351451c739ad71a371f9dbc53eb6
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-06-29 16:01:20 -0700

    062922A

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/03_add_actor.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/03_add_actor.sql
@@ -1,10 +1,6 @@
 -- 3. Did you know Eric Stoltz was originally cast as Marty McFly in "Back to the Future"?
 --Add Eric Stoltz to the list of actors for "Back to the Future" (1 row) 105 movie 3984917
-INSERT INTO person(person_name, person_id)
-VALUES('Eric Stoltz', 3984917);
 
 INSERT INTO movie_actor(movie_id, actor_id)
-VALUES (105, 3984917);
+VALUES (105, 7036)
 
-SELECT * FROM movie_actor
-WHERE movie_id = 105
