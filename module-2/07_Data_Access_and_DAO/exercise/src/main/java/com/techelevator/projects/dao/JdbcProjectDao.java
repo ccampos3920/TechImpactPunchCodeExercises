a2a51f3e9204494e1abe7d26b4f6998e0c3b6b94
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-07-06 14:58:30 -0700

    070622A

--- module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcProjectDao.java
+++ module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcProjectDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import com.techelevator.projects.model.Employee;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
@@ -20,22 +21,57 @@ public class JdbcProjectDao implements ProjectDao {
 
 	@Override
 	public Project getProject(int projectId) {
-		return new Project(0, "Not Implemented Yet", null, null);
+		Project project = null;
+		String sql = "SELECT * FROM project WHERE project_id = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, projectId);
+	while(results.next()){
+		project = mapRowToProject(results);
+	}
+	return project;
 	}
 
 	@Override
 	public List<Project> getAllProjects() {
-		return new ArrayList<>();
+		List<Project> project = new ArrayList<>();
+		String sql = "SELECT * FROM project;";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		while(results.next()){
+			project.add(mapRowToProject(results));
+		}
+		return project;
 	}
 
 	@Override
 	public Project createProject(Project newProject) {
-		return null;
+
+		String sql = "INSERT INTO project(project_id, name, from_date, to_date) "+
+				"VALUES(?,?,?,?)";
+
+		int createProject = jdbcTemplate.update(sql, newProject.getId(), newProject.getName(),
+				newProject.getFromDate(), newProject.getToDate());
+	return newProject;
 	}
+
 
 	@Override
 	public void deleteProject(int projectId) {
+		String sql = "DELETE FROM project_employee WHERE project_id = ?; DELETE FROM project WHERE project_id = ?";
+		jdbcTemplate.update(sql, projectId, projectId);
 
+	}
+
+	private Project mapRowToProject(SqlRowSet rowSet) {
+		Project project = new Project();
+		project.setId(rowSet.getInt("project_id"));
+		project.setName(rowSet.getString("name"));
+
+		if (rowSet.getDate("from_date") != null) {
+			project.setFromDate(rowSet.getDate("from_date").toLocalDate());
+		}
+		if (rowSet.getDate("to_date") != null) {
+			project.setToDate(rowSet.getDate("to_date").toLocalDate());
+		}
+		return project;
 	}
 	
 
