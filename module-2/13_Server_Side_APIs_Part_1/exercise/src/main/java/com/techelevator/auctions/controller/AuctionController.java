4701719c0374002916954de73f49d4fe495cb481
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-07-12 11:52:04 -0700

    071222A

--- module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -20,13 +20,33 @@ public class AuctionController {
 
 
     @RequestMapping(method = RequestMethod.GET )
-    public List<Auction> list() {
-        return dao.list();
+    public List<Auction> list(@RequestParam (defaultValue = "") String title_like, @RequestParam(defaultValue = "0") double currentBid_lte){
+        if(currentBid_lte > 0 && !title_like.equals("")){
+            return dao.searchByTitleAndPrice(title_like, currentBid_lte);
+        }
+
+        else if (!title_like.equals("")) {
+            return dao.searchByTitle(title_like);
+        }
+        else if(currentBid_lte > 0){
+            return dao.searchByPrice(currentBid_lte);
+        }
+
+        else {
+            return dao.list();
+        }
+
     }
 
-    @RequestMapping(method = RequestMethod.GET )
-    public Auction get(int id) {
+    @RequestMapping(path = "/{id}",method = RequestMethod.GET )
+    public Auction get(@PathVariable int id) {
             return dao.get(id);
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction create(@RequestBody Auction auction){
+        return dao.create(auction);
+    }
+
 }
 
