645214555abb550e2efde1d8fc3e92c9175ef311
Punchcode <punchcode@Punchcodes-MacBook-Pro-2.local>
2022-07-18 19:49:46 -0700

    071822A

--- module-2/Cat_Card_Capstone/exercise/src/main/java/com/techelevator/services/RestCatFactService.java
+++ module-2/Cat_Card_Capstone/exercise/src/main/java/com/techelevator/services/RestCatFactService.java
@@ -1,16 +1,22 @@
+
 package com.techelevator.services;
 
+import com.techelevator.model.CatFact;
 import org.springframework.stereotype.Component;
 
-import com.techelevator.model.CatFact;
+import com.techelevator.model.CatPic;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatFactService implements CatFactService {
+	public static final String API_BASE_URL = "https://cat-data.netlify.app/api/facts/random";
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@Override
-	public CatFact getFact() {
-		// TODO Auto-generated method stub
-		return null;
+	public CatFact getFact() throws RestClientResponseException {
+		CatFact fact = restTemplate.getForObject(API_BASE_URL, CatFact.class);
+		return fact;
 	}
 
 }
--- module-2/Cat_Card_Capstone/exercise/src/main/java/com/techelevator/services/RestCatPicService.java
+++ module-2/Cat_Card_Capstone/exercise/src/main/java/com/techelevator/services/RestCatPicService.java
@@ -1,16 +1,22 @@
+
 package com.techelevator.services;
 
+import com.techelevator.model.CatFact;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatPic;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatPicService implements CatPicService {
+	public static final String API_BASE_URL = "https://cat-data.netlify.app/api/pictures/random";
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@Override
-	public CatPic getPic() {
-		// TODO Auto-generated method stub
-		return null;
+	public CatPic getPic() throws RestClientResponseException {
+		CatPic pic = restTemplate.getForObject(API_BASE_URL, CatPic.class);
+		return pic;
 	}
 
-}	
+}
